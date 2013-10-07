e0dd161c97a85a3d20d9be46901367ddddffa8dc
AdminSibylla <paul.franceschi@yahoo.fr>
2013-10-07 21:53:52 +0200

    problem solved
    problem solved with the help of taazz from the Lazarus forum

--- unit1.pas
+++ unit1.pas
@@ -17,22 +17,20 @@ type
     BitBtn2: TBitBtn;
     BitBtn3: TBitBtn;
     BitBtn4: TBitBtn;
-    BitBtn5: TBitBtn;
     BitBtn6: TBitBtn;
-    BitBtn7: TBitBtn;
     CheckListBox1: TCheckListBox;
     CheckListBox2: TCheckListBox;
+    CheckListBox6: TCheckListBox;
+    CheckListBox7: TCheckListBox;
     CheckListBox3: TCheckListBox;
-    CheckListBox4: TCheckListBox;
     Label1: TLabel;
     Label2: TLabel;
+    Label3: TLabel;
     procedure BitBtn1Click(Sender: TObject);
     procedure BitBtn2Click(Sender: TObject);
     procedure BitBtn3Click(Sender: TObject);
     procedure BitBtn4Click(Sender: TObject);
-    procedure BitBtn5Click(Sender: TObject);
     procedure BitBtn6Click(Sender: TObject);
-    procedure BitBtn7Click(Sender: TObject);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
   private
@@ -43,7 +41,7 @@ type
 
 var
   Form1: TForm1;
-  slBuf, slPivot: TStringList;
+  slBuf: TStringList;
 
   function BinSearch(Strings: TStringList; SubStr: string; iFirst:integer;iLast:integer): Integer;
   function LowercaseFirst(const s:string):string;
@@ -54,30 +52,33 @@ implementation
 
 {$R *.lfm}
 
+
 function BinSearch(Strings: TStringList; SubStr: string; iFirst:integer;iLast:integer): Integer;
 // Binary search.
 // Caution: 1) No doubles in dic. 2) Dic must be sorted.
 // Problems : 'ciò' > 'città' ; 'ciò' > 'cit' ; 'ciò' > 'cip'
+// Binary search uses > comparison and =, while naive search only uses = operator.
 var
   iPivot: Integer;
   bFound: Boolean;
 begin
- slPivot.Clear;
  bFound  := False; //Initializes the bFound flag (Not bFound yet)
  Result := -1; //Initializes the Result
  //If iFirst > iLast then searched item doesn't exist
  //If item is bFound the loop stops
   while (iFirst <= iLast) and (not bFound) do
   begin
-    iPivot := (iFirst + iLast) div 2; // trunc(iFirst + iLast) gives the same result.
-    slPivot.Add(Inttostr(iPivot) +':' + Strings[iPivot]);
-    if (Strings[iPivot] = SubStr) then // UTF8CompareText(Strings[iPivot], SubStr) < 0 then
+   iPivot := (iFirst + iLast) div 2; // trunc(iFirst + iLast) gives the same result.
+//    if (Strings[iPivot] = SubStr) then // UTF8CompareText(Strings[iPivot], SubStr) < 0 then
+   if Strings[iPivot] = SubStr then // UTF8CompareText(Strings[iPivot], SubStr) < 0 then
     begin
       bFound  := True;
       Result := iPivot;
     end
-    else if (Strings[iPivot] > SubStr) then // UTF8CompareText(Strings[iPivot], SubStr) > 0 then //
-      iLast := iPivot - 1
+//    else if (Strings[iPivot] > SubStr) then // UTF8CompareText(Strings[iPivot], SubStr) > 0 then
+//    else if UTF8CompareText(Strings[iPivot], SubStr) > 0 then
+    else if WideCompareText(UTF8Decode(Strings[iPivot]), UTF8Decode(SubStr)) > 0 then
+    iLast := iPivot - 1
     else
       iFirst := iPivot + 1;
   end;
@@ -139,7 +140,7 @@ var
  i:integer;
 begin
  for i:= 0 to Checklistbox1.Count-2 do
-  if Checklistbox1.Items[i] < Checklistbox1.Items[i+1] then Checklistbox3.Items.Add('<');
+  if Checklistbox1.Items[i] < Checklistbox1.Items[i+1] then CheckListBox6.Items.Add('<');
 end;
 
 procedure TForm1.BitBtn4Click(Sender: TObject);
@@ -147,63 +148,42 @@ var
 i, iCount, iIndex:integer;
 s:string;
 begin
- for i:= 0 to Checklistbox2.Count-1 do
-  if Checklistbox2.Checked[i] then Checklistbox2.Checked[i] := false;
+ for i:= 0 to Checklistbox3.Count-1 do
+  if Checklistbox3.Checked[i] then Checklistbox3.Checked[i] := false;
 for i:= 0 to Checklistbox1.Count-1 do
  slBuf.Add(Checklistbox1.Items[i]);  // slBuf.Add(Lowercase(Checklistbox1.Items[i]));
-for i:= 0 to Checklistbox2.Count-1 do
+for i:= 0 to Checklistbox3.Count-1 do
 begin
- s := Checklistbox2.Items[i];  // Lowercase(Checklistbox2.Items[i]);
+ s := Checklistbox3.Items[i];  // Lowercase(Checklistbox2.Items[i]);
  iIndex := NaiveSearch(slBuf, s);
  if (iIndex >= 0) then
-   Checklistbox2.Checked[i] := true;
+   Checklistbox3.Checked[i] := true;
 end;
 iCount := 0;
-for i:= 0 to Checklistbox2.Count-1 do
- if Checklistbox2.Checked[i] = true then
+for i:= 0 to Checklistbox3.Count-1 do
+ if Checklistbox3.Checked[i] = true then
   Inc(iCount);
-Label1.Caption := Inttostr(iCount) + '/' + Inttostr(Checklistbox2.Count);
-{ Checklistbox3.Clear;
+Label3.Caption := Inttostr(iCount) + '/' + Inttostr(Checklistbox3.Count);
+{ CheckListBox6.Clear;
 for i:= 0 to slBuf.Count-1 do
- Checklistbox3.Items.Add(slBuf[i]+ ':' + Inttostr(Length(slBuf[i]))); }
+ CheckListBox6.Items.Add(slBuf[i]+ ':' + Inttostr(Length(slBuf[i]))); }
 end;
 
-procedure TForm1.BitBtn5Click(Sender: TObject);
-var
- i, iIndex:integer;
-begin
- slBuf.Clear;
- slPivot.Clear;
- for i:= 0 to Checklistbox1.Count-1 do
-  slBuf.Add(Checklistbox1.Items[i]);
- iIndex := BinSearch(slBuf, Checklistbox2.Items[Checklistbox2.ItemIndex], 0, slBuf.Count-1);
- if (iIndex >= 0) then // Checked if found.
-  Checklistbox2.Checked[Checklistbox2.ItemIndex] := true;
- Checklistbox4.Clear;
- for i:= 0 to slPivot.Count-1 do
-  Checklistbox4.Items.Add(slPivot[i]);
-end;
 
 procedure TForm1.BitBtn6Click(Sender: TObject);
 begin
-  ClipBoard.SetTextBuf(Pchar(Checklistbox4.Items.Text));
+  ClipBoard.SetTextBuf(Pchar(CheckListBox7.Items.Text));
 end;
 
-procedure TForm1.BitBtn7Click(Sender: TObject);
-begin
-  if CompareStr('ciò', 'cita') < 0 then Label2.Caption := '<' else Label2.Caption:= '>';// 'ciò' < 'cita' then Label2.Caption:= '<' else Label2.Caption:= '>';
-end;
 
 procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
 begin
  slBuf.Free;
- slPivot.Free;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
  slBuf := TStringList.Create;
- slPivot := TStringList.Create;
 end;
 
 
